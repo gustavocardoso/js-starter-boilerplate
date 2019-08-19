0b1a0fa9e2f91d68cbdc9c0ac18ac545a46e1f31
Gustavo Cardoso <gustavocardoso@gmail.com>
2019-08-18 18:12:09 -0700

    Add config files

---
+++ README.md
@@ -0,0 +1,3 @@
+# JS Starter Boilerplate
+
+A simple boilerplate to start your simple JS project.
---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  context: path.resolve(__dirname, './src'),
+  entry: {
+    app: './main.js'
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './dist')
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, './dist')
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader'
+          }
+        ]
+      }
+    ]
+  }
+}
